720d58948008294a38bea74d991b809458c89768
mikestem <michael.a.stem@gmail.com>
2022-12-10 10:05:39 -0500

    added num of char prompt

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 var generateBtn = document.querySelector("#generate")
 function generatePassword(){
   var numChar = ["0", "1", "2", "3", "4", "5", "6", "7" , "8", "9"];
+  var capsChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "[", "]", "{", "}", "<", ">", ";", ":", "?", "/", "."]
+  var possChar = [];
+
+  numChar = prompt("Password must be between 8-128 characters. How many would you like?");
+  if (numChar <8 || numChar > 128) {
+    alert "Please choose number between 8-128.";
+  } else if (isNaN(numChar)){
+    numChar = prompt ("Enter a valid number");
+  }
+  else{
+    alert("Password will be " + numChar + " characters long");
+  }
 }
 
 // // Write password to the #password input
